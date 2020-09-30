4ff20f6701138a003e4081115d4439d40e2ef5dd
Yannistan <tanis.ioannis@gmail.com>
2020-09-30 10:42:34 +0200

    final version

---
+++ src/brouillon.js
@@ -0,0 +1,30 @@
+/*
+async function fonctionAsynchroneOk() {
+ // équivaut à :
+ return Promise.resolve('résultat');
+ 
+}
+fonctionAsynchroneOk().then(console.log) // log "résultat"
+
+async function fonctionAsynchroneKo() {
+ // équivaut à :
+  return Promise.reject(new Error('erreur'));
+ //throw new Error('erreur');
+}
+fonctionAsynchroneKo().catch(err => console.log(err.message)) // log "erreur" 
+*/
+
+//*async function getNombreAsynchrone1() {/* traitement asynchrone (e.g. appel d’une API HTTP) */} 
+//async function getNombreAsynchrone2() {/* traitement asynchrone (e.g. appel d’une API HTTP) */}
+
+
+
+/*
+async function getNombreAsynchrone1() {
+ const nombre1 =  10;
+ const nombre2 =  11
+ return nombre1 + nombre2;
+} 
+*/
+
+//getAdditionAsynchrone().then(console.log)
--- src/showDirFiles.js
+++ src/showDirFiles.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+
 if (process.argv.length !== 3) {
     console.log('usage: showDirFiles.js dir')
     process.exit(1)
@@ -8,10 +9,12 @@ let dir = process.argv[2];
 
 fs.readdir(dir, function (err, files) {
     if (err) {
-        return console.log('Unable to scan directory: ' + err);
+        return console.log('Unable to open directory: ', err);
     }
 
     files.forEach(function (file) {
-        console.log(file);
+        let path = process.argv[2]+file
+        fs.readFile(path, 'utf-8', (err, data) => {console.log(data)}) 
+       // console.log(file);
     });
 });
